08c62a77cdbfee2ba39cb39e7d32f2d7d5f29d8b
Rumonz <lone_assassin@mail.ru>
2022-12-17 00:51:47 +0300

    Выполнил задание 2 Домашнего задания 1

--- src/Автомобиль.java
+++ src/Автомобиль.java
@@ -5,7 +5,18 @@ public class Автомобиль {
     String color;
     int year;
     String country;
-     public String toString(){
+
+    Автомобиль(String brand,String model,int year,String country,String color,double engineVolume ) {
+        this.brand=brand;
+        this.color = color;
+        this.country = country;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.year =year;
+
+    }
+
+    public String toString(){
          return "Автомобиль - "+brand+" "+model+"."+" Год выпуска - "+year+"."+" Страна где собрано - "+country+"."+" Цвет  "+color+"."+" Объём двигателя - "+engineVolume+" литров.";
     }
 }
